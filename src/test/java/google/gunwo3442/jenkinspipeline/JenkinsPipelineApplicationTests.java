8b9638997ec5eb2755a44e375d4ea9db5e2bb5be
gunwoda <gunwo3442@gmail.com>
2024-11-08 09:31:05 +0900

    initial setting

---
+++ src/test/java/google/gunwo3442/jenkinspipeline/JenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package google.gunwo3442.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
